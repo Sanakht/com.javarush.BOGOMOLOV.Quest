f742179356a2994ea07e5586350eed9b27d3ee4b
Yaroslav Bogomolov <bogomolov200@yandex.ru>
2025-09-28 14:23:03 +0500

    Working

--- src/main/java/com/javarush/bogomolov/quest/servlets/StartServlet.java
+++ src/main/java/com/javarush/bogomolov/quest/servlets/StartServlet.java
@@ -1,27 +1,111 @@
-package com.javarush.bogomolov.quest;
+package com.javarush.bogomolov.quest.servlets;
 
-import java.io.*;
+import com.javarush.bogomolov.quest.constants.AppConstants;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
-    private String message;
+import java.io.IOException;
 
-    public void init() {
-        message = "Hello World!";
+@WebServlet(name = "start-Servlet", value = "/start")
+public class StartServlet extends HttpServlet {
+
+    private static String question;
+    private static String answer;
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        HttpSession session = request.getSession();
+
+
+        if(session.getAttribute(AppConstants.SessionParameters.START) == null) {
+            session.setAttribute(AppConstants.SessionParameters.START, true);
+            session.setAttribute(AppConstants.SessionParameters.WEAPON, false);
+            session.setAttribute(AppConstants.SessionParameters.DEAD, false);
+            session.setAttribute(AppConstants.SessionParameters.WIN, false);
+            session.setAttribute(AppConstants.SessionParameters.QUESTION, null);
+//            session.setAttribute(AppConstants.SessionParameters.ANSWER, answer);
+        }
+
+
+        getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        HttpSession session = request.getSession();
+
+        answer = (String) request.getParameter("answer");
+
+        if(answer != null) {
+            switch(answer) {
+                case "ANSWER_1_1" -> {
+                    question = AppConstants.Question.QUESTION_2;
+                    session.setAttribute(AppConstants.SessionParameters.QUESTION, question);
+                }
+                case "ANSWER_1_2" -> {
+                    question = AppConstants.Question.QUESTION_SLEEP_END;
+                    session.setAttribute(AppConstants.SessionParameters.QUESTION, question);
+                    session.setAttribute(AppConstants.SessionParameters.DEAD, true);
+                }
+                case "ANSWER_1_3" -> {
+                    question = AppConstants.Question.QUESTION_3;
+                    session.setAttribute(AppConstants.SessionParameters.QUESTION, question);
+                }
+                case "ANSWER_2_1" -> {
+                    question = AppConstants.Question.QUESTION_WAIT_WIN;
+                    session.setAttribute(AppConstants.SessionParameters.QUESTION, question);
+                    session.setAttribute(AppConstants.SessionParameters.WIN, true);
+                }
+                case "ANSWER_2_2" -> {
+                    question = AppConstants.Question.QUESTION_4;
+                    session.setAttribute(AppConstants.SessionParameters.QUESTION, question);
+                    session.setAttribute(AppConstants.SessionParameters.WEAPON, true);
+                }
+                case "ANSWER_2_3" -> {
+                    question = AppConstants.Question.QUESTION_3;
+                    session.setAttribute(AppConstants.SessionParameters.QUESTION, question);
+                }
+                case "ANSWER_3_1" ->{
+                    question = AppConstants.Question.QUESTION_RUN_IN_HOUSE_END;
+                    session.setAttribute(AppConstants.SessionParameters.QUESTION, question);
+                    session.setAttribute(AppConstants.SessionParameters.DEAD, true);
+                }
+                case "ANSWER_3_2" ->{
+                    question = AppConstants.Question.QUESTION_CLOSE_DOOR_END;
+                    session.setAttribute(AppConstants.SessionParameters.QUESTION, question);
+                    session.setAttribute(AppConstants.SessionParameters.DEAD, true);
+                }
+                case "ANSWER_3_3" ->{
+                    question = AppConstants.Question.QUESTION_RUN_OUT_HOUSE_WIN;
+                    session.setAttribute(AppConstants.SessionParameters.QUESTION, question);
+                    session.setAttribute(AppConstants.SessionParameters.WIN, true);
+                }
+                case "ANSWER_4_1" ->{
+                    question = AppConstants.Question.QUESTION_CLOSE_DOOR_WITH_WEAPON_END;
+                    session.setAttribute(AppConstants.SessionParameters.QUESTION, question);
+                    session.setAttribute(AppConstants.SessionParameters.DEAD, true);
+                }
+                case "ANSWER_4_2" ->{
+                    question = AppConstants.Question.QUESTION_ATTACK_WIN;
+                    session.setAttribute(AppConstants.SessionParameters.QUESTION, question);
+                    session.setAttribute(AppConstants.SessionParameters.WIN, true);
+                }
+            }
+        }
+        response.sendRedirect("index.jsp");
+
+
+
+
+
+
+
+
     }
 
-    public void destroy() {
-    }
+
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,12 +1,154 @@
+<%@ page import="com.javarush.bogomolov.quest.constants.AppConstants" %>
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
 <!DOCTYPE html>
 <html>
 <head>
-  <title>JSP - Hello World</title>
+  <title>JSP - Start</title>
 </head>
 <body>
-<h1><%= "Hello World!" %></h1>
+
+<h1>
+  <c:if test="${sessionScope.start != true}" >
+    <%=
+    AppConstants.Question.SHOW
+    %>
+    <br/>
+    <%=
+    AppConstants.Question.DESCRIPTION
+    %>
+    <br/>
+    <%=
+    AppConstants.Question.RULES
+    %>
+  </c:if>
+</h1>
+
+<c:set var="question1" value="<%=AppConstants.Question.QUESTION_1%>"/>
+<c:set var="question2" value="<%=AppConstants.Question.QUESTION_2%>"/>
+<c:set var="question3" value="<%=AppConstants.Question.QUESTION_3%>"/>
+<c:set var="question4" value="<%=AppConstants.Question.QUESTION_4%>"/>
+
+
+<c:set var="start" value="<%= session.getAttribute(AppConstants.SessionParameters.START)%>"/>
+<c:set var="weapon" value="<%= session.getAttribute(AppConstants.SessionParameters.WEAPON)%>"/>
+<c:set var="dead" value="<%= session.getAttribute(AppConstants.SessionParameters.DEAD)%>"/>
+<c:set var="win" value="<%= session.getAttribute(AppConstants.SessionParameters.WIN)%>"/>
+<c:set var="question" value="<%= session.getAttribute(AppConstants.SessionParameters.QUESTION)%>"/>
+<c:set var="answer" value="<%= session.getAttribute(AppConstants.SessionParameters.ANSWER)%>"/>
+
+<c:set var="session" value="<%= request.getSession().toString()%>"/>
+
+
+<c:choose>
+  <c:when test="${sessionScope.start != true}">
+    <p>Сессия не запущена</p>
+    <form method="get" action="start">
+      <button type="submit">Запустить квест</button>
+    </form>
+  </c:when>
+<%--  <c:when test="${sessionScope.start == true}">--%>
+<%--    <p>Сессия запущена</p>--%>
+<%--    <form method="post" action="restart">--%>
+<%--      <button type="submit">Сбросить квест</button>--%>
+<%--    </form>--%>
+<%--  </c:when>--%>
+  <c:when test="${sessionScope.dead == true}">
+    <p>${sessionScope.question}</p>
+    <p>Вы умерли, хотите перезапустить сессию?</p>
+    <form method="post" action="restart">
+      <button type="submit">Сбросить квест</button>
+    </form>
+  </c:when>
+  <c:when test="${sessionScope.win == true}">
+    <p>${sessionScope.question}</p>
+    <p>Вы прошли, хотите перезапустить сессию?</p>
+    <form method="post" action="restart">
+      <button type="submit">Сбросить квест</button>
+    </form>
+  </c:when>
+</c:choose>
+
+
 <br/>
-<a href="hello-servlet">Hello Servlet</a>
+
+<c:if test="${sessionScope.start == true && sessionScope.dead != true && sessionScope.win != true}">
+  <c:set var="chek" value="${true}"/>
+</c:if>
+
+
+
+<c:if test = "${sessionScope.question == null && chek}">
+  <p>
+    <%=
+    AppConstants.Question.QUESTION_1
+    %>
+  </p>
+  <form method="post" action="/start">
+    <input type="radio" name="answer" value="ANSWER_1_1"> <%= AppConstants.Answer.ANSWER_1_1%><br/>
+    <input type="radio" name="answer" value="ANSWER_1_2"> <%= AppConstants.Answer.ANSWER_1_2%><br/>
+    <input type="radio" name="answer" value="ANSWER_1_3"> <%= AppConstants.Answer.ANSWER_1_3%><br/>
+    <button type="submit"> Подтвердить </button><br/>
+  </form>
+</c:if>
+
+<c:if test = "${sessionScope.question == question2 && chek}">
+  <p>
+    <%=
+    AppConstants.Question.QUESTION_2
+    %>
+  </p>
+  <form method="post" action="/start">
+    <input type="radio" name="answer" value="ANSWER_2_1"> <%= AppConstants.Answer.ANSWER_2_1%><br/>
+    <input type="radio" name="answer" value="ANSWER_2_2"> <%= AppConstants.Answer.ANSWER_2_2%><br/>
+    <input type="radio" name="answer" value="ANSWER_2_3"> <%= AppConstants.Answer.ANSWER_2_3%><br/>
+    <button type="submit"> Подтвердить </button><br/>
+  </form>
+</c:if>
+
+<c:if test = "${sessionScope.question == question3 && chek}">
+  <p>
+    <%=
+    AppConstants.Question.QUESTION_3
+    %>
+  </p>
+  <form method="post" action="/start">
+    <input type="radio" name="answer" value="ANSWER_3_1"> <%= AppConstants.Answer.ANSWER_3_1%><br/>
+    <input type="radio" name="answer" value="ANSWER_3_2"> <%= AppConstants.Answer.ANSWER_3_2%><br/>
+    <input type="radio" name="answer" value="ANSWER_3_3"> <%= AppConstants.Answer.ANSWER_3_3%><br/>
+    <button type="submit"> Подтвердить </button><br/>
+  </form>
+</c:if>
+
+<c:if test = "${sessionScope.question == question4 && chek}">
+  <p>
+    <%=
+    AppConstants.Question.QUESTION_4
+    %>
+  </p>
+  <form method="post" action="/start">
+    <input type="radio" name="answer" value="ANSWER_4_1"> <%= AppConstants.Answer.ANSWER_4_1%><br/>
+    <input type="radio" name="answer" value="ANSWER_4_2"> <%= AppConstants.Answer.ANSWER_4_2%><br/>
+    <button type="submit"> Подтвердить </button><br/>
+  </form>
+</c:if>
+
+
+
 </body>
 </html>
+
+
+<p>
+  Атрибуты сессии: <br/>
+  session: ${session} <br/>
+  start: ${start} <br/>
+  weapon: ${weapon} <br/>
+  dead: ${dead} <br/>
+  win: ${win} <br/>
+  question: ${question} <br/>
+<%--  answer: ${answer}<br/>--%>
+</p>
+
+<br/>
